741cf47b5c7c43a02a4c415d35b7505ef9d3d999
Benjamin Richards <richardsbenjaminpeter@gmail.com>
2026-04-05 14:32:44 +0200

    Changes.

--- healdit/models/updown/__init__.py
+++ healdit/models/updown/__init__.py
@@ -4,3 +4,4 @@ from healdit.models.updown.updowngraph import UpDown
 from healdit.models.updown.updownvaewindow import UpDownVAEWindow
 from healdit.models.updown.updownvaewindownonresid import UpDownVAEWindowNonResid
 from healdit.models.updown.updownvaewindowbatchnorm import UpDownVAEWindowBatchNorm
+from healdit.models.updown.updownvaedct import UpDownVAEDCT
